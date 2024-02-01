95126e8f2261a30b4a4280810b77f09f2cda4630
san0123 <enteroa.j@gmail.com>
2024-02-01 12:51:34 +0900

    Update ncc.sh

--- ncc.sh
+++ ncc.sh
@@ -4,7 +4,7 @@ basedir=/opt/ncc
 
 
 cd $basedir
-/bin/python3.9 $basedir/run_firefox.py
+/bin/python3 $basedir/run_firefox.py
 TARGET_PID=$(ps -ef|awk '/firefox/&&/headless/&&!/awk/{print $2}')
 if [[ $TARGET_PID -gt 1 ]];then
   kill -9 $TARGET_PID
