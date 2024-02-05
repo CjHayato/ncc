8736186545a236c96ab5d8b33fc0db547cf4efab
CJ.Hayato <rabbitlcj@me.com>
2024-02-05 14:16:47 +0900

    Update comments

--- run_firefox.py
+++ run_firefox.py
@@ -10,11 +10,11 @@ from selenium.webdriver.common.by import By
 
 class naver_coin_scraper:
     def __init__(self):
-        ####### 여기 있는 정보 OS 에따라 수정 될 수 있다. #######        # geckodriver 경로를 아래에 기입
-        self.se = Service(executable_path='/usr/local/bin/geckodriver')
+        ####### 여기 있는 정보 OS 에따라 수정 될 수 있다. #######
+        self.se = Service(executable_path='/usr/local/bin/geckodriver')  # geckodriver 경로를 아래에 기입
         #########################################################
         self.visited_urls_file='visited_urls.txt'
-        try:                                                             # Read visited URLs from file
+        try:                                                             # 방문 기록을 파일에서 읽어 온다
             with open(self.visited_urls_file, 'r') as file:
                 self.visited_urls = set(file.read().splitlines())
         except FileNotFoundError:
@@ -27,28 +27,28 @@ class naver_coin_scraper:
         driver = webdriver.Firefox(service=self.se, options=firefox_options)
         driver.get('https://naver.com')
         current_window_handle = driver.current_window_handle             # 현재 열려 있는 창 가져오기
-        # <a href class='MyView-module__link_login___HpHMW'> 을 네이버 로그인창으로 인식
+        # <a href class='MyView-module__link_login___HpHMW'> 을 네이버 로그인 창으로 인식
         driver.find_element(By.XPATH, "//a[@class='MyView-module__link_login___HpHMW']").click()
-        new_window_handle = None                                         # 새롭게 생성된 탭의 핸들을 찾습니다
+        new_window_handle = None                                         # 새롭게 생성된 탭의 핸들을 찾음
         for handle in driver.window_handles:
             if handle != current_window_handle:
                 new_window_handle = handle
                 break
             else:
                 new_window_handle = handle
-        driver2 = driver.switch_to.window(new_window_handle)             # 새탭으로 변경.
+        driver2 = driver.switch_to.window(new_window_handle)             # 새탭으로 변경
         username = driver2.find_element(By.NAME, 'id')
         pw = driver2.find_element(By.NAME, 'pw')
-        username.click()                                                 # 네이버 ID 입력
+        username.click()                                                 # 네이버 로그인 전용 ID 입력
         driver2.execute_script("arguments[0].value = arguments[1]", username, config.input_id)
         time.sleep(1)
-        pw.click()                                                       # 네이버 PW 입력
+        pw.click()                                                       # 네이버 로그인 전용 PW 입력
         driver2.execute_script("arguments[0].value = arguments[1]", pw, config.input_pw)
         time.sleep(1)
         driver2.find_element(By.CLASS_NAME, "btn_login").click()
         time.sleep(1)
         for link in campaign_links:
-            driver.get(link)                                            # 네이버 캡페인 접속
+            driver.get(link)                                            # 네이버 캠페인 접속
             try:
                 result = driver.switch_to.alert
                 print(result.text)
@@ -70,14 +70,14 @@ class naver_coin_scraper:
         campaign_links = set()
         if len(posts) != 0:
             for link in posts:
-                if link in self.visited_urls:                           # 기록에 따라 방문 했던곳은 넘어간다.
+                if link in self.visited_urls:                           # 기록에 따라 방문 했던 곳은 넘어 간다.
                     continue
                 res = requests.get(link)
                 inner_soup = BeautifulSoup(res.text, 'html.parser')
-                for a_tag in inner_soup.find_all('a', href=True):       # 아이클에서 링크주소(a href)를 가져온다.
+                for a_tag in inner_soup.find_all('a', href=True):       # 아티클에서 링크 주소(a href)를 가져 온다.
                     campaign_link = a_tag.get_text().strip()
                     if ('campaign2-api.naver.com' in campaign_link or 'ofw.adison.co' in campaign_link) and campaign_link not in campaign_links:
-                        campaign_links.add(campaign_link)               # 캠페인주소일 경우 목록에 추가한다.
+                        campaign_links.add(campaign_link)               # 캠페인 주소일 경우 목록에 추가 한다.
         return campaign_links
 
     def post_scrap(self):
@@ -110,7 +110,7 @@ class naver_coin_scraper:
         campaign_links = naver_coin_scraper.campaign_scrap(self, posts, base_url)
         print("searched naver campaign:", len(campaign_links))
         if len(campaign_links) >= 1:
-            naver_coin_scraper.get_coin(self, campaign_links)           # firefox를 통한 네이버 접속ㄱㄱ
+            naver_coin_scraper.get_coin(self, campaign_links)           # firefox를 통한 네이버 접속 시작
             self.visited_urls = posts
             with open(self.visited_urls_file, 'w') as file:
                 for url in self.visited_urls:
