e05aa0b6e3aad59ae427e7cf8a282e152f76b8a3
SooHwan Jang <enteroa.j@gmail.com>
2024-06-12 11:48:01 +0900

    Update run_firefox.py
    request header - user-agent 삽입

--- run_firefox.py
+++ run_firefox.py
@@ -14,11 +14,13 @@ from selenium.webdriver.firefox.service import Service
 def avoid_overlab():
     sys.argv[0]
     pid_lock_file = sys.argv[0] + '.pid'
-    try: fcntl.lockf(open(pid_lock_file, 'w'), fcntl.LOCK_EX | fcntl.LOCK_NB)
+    try:
+        fcntl.lockf(open(pid_lock_file, 'w'), fcntl.LOCK_EX | fcntl.LOCK_NB)
     except IOError:
         print('이미 실행 중입니다')
         sys.exit(1)
-    finally: os.remove(pid_lock_file)
+    finally:
+        os.remove(pid_lock_file)
 
 class naver_coin_scraper:
     def __init__(self):
@@ -26,6 +28,7 @@ class naver_coin_scraper:
         self.gecko = '/usr/local/bin/geckodriver'
         #########################################################
         self.visited_urls_file='visited_urls.txt'
+        self.ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
         try:                                                       # 방문 기록을 파일에서 읽어 온다
             with open(self.visited_urls_file, 'r') as file:
                 self.visited_urls = set(file.read().splitlines())
@@ -40,13 +43,12 @@ class naver_coin_scraper:
         f_opts.add_argument("--disable-gpu")                       # GPU 가속 비활성화
         f_opts.add_argument("--no-sandbox")                        # 샌드박스 모드 비활성화
         f_opts.add_argument("--disable-blink-features=AutomationControlled")
-        ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0"
-        f_opts.set_preference("general.useragent.override", ua)
-        for nid, npw in config.naver_login_info.items():
+        f_opts.set_preference("general.useragent.override", self.ua)
+        for nid, npw in config.naver_login_info.items():           # config에서 선언된 더미 아이디는 건너 뛴다
             if nid is None or nid == "" or nid == "naver_ID1" or nid == "naver_ID2" or nid == "naver_ID3":
                 continue
             else:
-                if sys.version_info.major == 3:
+                if sys.version_info.major == 3:                    # python 버전에 따라 selenium 버전이 바뀌고 사용법이 다르다
                     if sys.version_info.minor >= 10:
                         driver = webdriver.Firefox(service=Service(executable_path=self.gecko, log_output=os.devnull), options=f_opts)
                     elif 9 >= sys.version_info.minor >= 7:
@@ -55,11 +57,11 @@ class naver_coin_scraper:
                         driver = webdriver.Firefox(executable_path=self.gecko, service_log_path=os.devnull, options=f_opts)
                 try:
                     driver.get('https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/')
-                    driver.implicitly_wait(10)
+                    driver.implicitly_wait(10)                     # 로그인 페이지 로딩 완료를 기다린다
                     driver.execute_script("document.getElementsByName('id')[0].value=\'" + nid + "\'")
                     driver.execute_script("document.getElementsByName('pw')[0].value=\'" + npw + "\'")
                     driver.find_element(By.CLASS_NAME, "btn_login").click()
-                    driver.implicitly_wait(10)
+                    driver.implicitly_wait(10)                     # 로그인후 로딩이 완료되길 기다린다
                     with open("scrap-link.logs", "a") as f:
                         f.write(str(time.strftime('%Y-%m-%d %H:%M:%S')) + ' naver login success.\n')
                     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
@@ -72,7 +74,7 @@ class naver_coin_scraper:
                             with open("scrap-link.logs", "a") as f:
                                 f.write(str(time.strftime('%Y-%m-%d %H:%M:%S')) + ' ' + result.text + '\n')
                             result.accept()
-                        except:                                    # 레이어 팜업 내용을 기록 한다.
+                        except:                                    # 레이어 팝업 내용을 기록 한다.
                             soup = BeautifulSoup(driver.page_source, 'html.parser')
                             if "div" in str(soup.find('div', class_="dim")):
                                 DIM = soup.find('div', class_="dim")
@@ -91,7 +93,7 @@ class naver_coin_scraper:
             for link in posts:
                 if link in self.visited_urls:                      # 기록에 따라 방문 했던 곳은 넘어 간다
                     continue
-                res = requests.get(link)
+                res = requests.get(link, headers={"User-Agent": self.ua})
                 inner_soup = BeautifulSoup(res.text, 'html.parser')
                 for a_tag in inner_soup.find_all('a', href=True):  # 아티클에서 링크 주소(a href)를 가져 온다
                     cl = a_tag.get_text().strip()
@@ -105,7 +107,7 @@ class naver_coin_scraper:
                             "https://www.clien.net/service/board/jirum",
                             "https://www.ppomppu.co.kr/zboard/zboard.php?id=coupon" ]
         for base_url in post_check_urls:
-            response = requests.get(base_url)
+            response = requests.get(base_url, headers={"User-Agent": self.ua})
             soup = BeautifulSoup(response.text, 'html.parser')
             post = set()
             if 'damoang.net' in base_url:                          # damoang: list-group-item - li
@@ -118,14 +120,14 @@ class naver_coin_scraper:
             if len(list_subject_links) != 0:
                 for span in list_subject_links:
                     a_tag = span.find('a', href=True)
-                    if a_tag and '네이버' in a_tag.text:           # 아티클의 URL을 조합한다
-                        try:
+                    if a_tag and '네이버' in a_tag.text:
+                        try:                                       # baseurl과 링크URL 을 조합해 캠페인에 추가 한다
                             post.add(str(base_url.split(url_split)[0]) + str(a_tag['href']))
                         except:
-                            post.add(str(a_tag['href']))
+                            post.add(str(a_tag['href']))           # damoang은 링크URL이 풀URL으로 나온다
                 try:
                     del url_split
-                except:
+                except NameError:
                     pass
             posts |= post                                          # set() + set()
             print("searched new article", len(post), "from: " + base_url)
